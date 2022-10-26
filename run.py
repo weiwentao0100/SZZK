313acd021a3f244d86b3045ba0602d544d7d0409
weiwentao <2569713531@qq.com>
2022-10-26 22:11:22 +0800

    将合同ID添加到类变量

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from testcases.cases import TestCase
 
 if __name__ == '__main__':
     testcase = TestCase("https://test12.ahweimeng.cn")
-    testcase.add_supplyList()
+    testcase.add_one_supplyList()
+    testcase.add_two_supplyList()
     testcase.add_project()
--- testcases/cases.py
+++ testcases/cases.py
@@ -18,6 +18,7 @@ class TestCase:
         self.host = host
         self.number = SetUtil().setup_autonumber()
         self.time = SetUtil().setup_datetime()
+        self.contract_id = self.add_contract()
 
     def get_token(self):
         """ 获取用户token """
@@ -67,7 +68,7 @@ class TestCase:
         # contract_id = response['body']['data']
         # return contract_id
 
-    def add_supplyList(self):
+    def add_one_supplyList(self):
         """ 新增供货清单 """
         url = self.host + "/admin/contract/supplyList/add"
         headers = {
@@ -75,7 +76,7 @@ class TestCase:
             "Content-Type": "application/json"
         }
         data = {
-            "contractId": self.add_contract(),  # 合同ID
+            "contractId": self.contract_id,  # 合同ID
             "amount": 55,  # 数量
             "name": "钢丝",  # 采购内容
             "remark": "钢丝采购",  # 备注
@@ -84,7 +85,27 @@ class TestCase:
             "unitPrice": 99  # 单价
         }
         response = self.request.post(url, headers=headers, json=data)
-        self.assert_util.assert_code(response['code'], 200)
+        self.assert_util.assert_in_body(response['code'], "'code': 0")
+        print(f"供货清单新增成功，采购内容为：{data['name']}, 返回body: {response['body']}")
+
+    def add_two_supplyList(self):
+        """ 新增供货清单 """
+        url = self.host + "/admin/contract/supplyList/add"
+        headers = {
+            "Authorization": self.get_token(),
+            "Content-Type": "application/json"
+        }
+        data = {
+            "contractId": self.contract_id,  # 合同ID
+            "amount": 88,  # 数量
+            "name": "废油",  # 采购内容
+            "remark": "废气采购",  # 备注
+            "spec": "95#",  # 规格
+            "unit": "升",  # 单位
+            "unitPrice": 8.1  # 单价
+        }
+        response = self.request.post(url, headers=headers, json=data)
+        self.assert_util.assert_in_body(response['code'], "'code': 0")
         print(f"供货清单新增成功，采购内容为：{data['name']}, 返回body: {response['body']}")
 
     def add_project(self):
@@ -95,33 +116,33 @@ class TestCase:
             "Content-Type": "application/json"
         }
         data = {
-            "name": "文涛的测试项目_" + self.number,  # 项目名称
-            "contractId": self.add_contract(),  # 合同ID
+            "name": "文涛的测试项目_1" + self.number,  # 项目名称
+            "contractId": self.contract_id,  # 合同ID
             "projectStartDate": self.time[0],  # 项目开始时间
             "projectEndDate": self.time[10],  # 项目结束时间
             "chargePersonDate": self.time[1],  # 项目总负责人完成时间
-            "chargePersonIds": "13041141,13041048",  # 项目总负责人ID
+            "chargePersonIds": "13041048,13041141,13041082",  # 项目总负责人ID
             "designerDate": self.time[2],  # 设计完工时间
-            "designerIds": "13041141,13041048",  # 设计人员ID
+            "designerIds": "13041048,13041141,13041082",  # 设计人员ID
             "productionPreparationDate": self.time[3],  # 生产准备部完工时间
-            "productionPreparationIds": "13041141,13041048",  # 生产准备部人员ID
+            "productionPreparationIds": "13041048,13041141,13041082",  # 生产准备部人员ID
             "purchaserDate": self.time[4],  # 采购完工时间
-            "purchaserIds": "13041141,13041048",  # 采购人员ID
+            "purchaserIds": "13041048,13041141,13041082",  # 采购人员ID
             "machiningDate": self.time[5],  # 加工完工时间
-            "machiningIds": "13041141,13041048",  # 加工队伍人员ID
+            "machiningIds": "13041048,13041141,13041082",  # 加工队伍人员ID
             "supplierDate": self.time[6],  # 供应商完工时间
-            "supplierIds": "13041157",  # 供应商ID
+            "supplierIds": "13041050,13041157,13041118",  # 供应商ID
             "rawMaterialManagerDate": self.time[7],  # 原材料/仓库管理员完工时间
-            "rawMaterialManagerIds": "13041141,13041048",  # 原材料/仓库管理员ID
+            "rawMaterialManagerIds": "13041141,13041048,13041082",  # 原材料/仓库管理员ID
             "siteServiceDate": self.time[8],  # 现场服务完工时间
-            "siteServiceIds": "13041141,13041048",  # 现场服务人员ID
+            "siteServiceIds": "13041141,13041048,13041082",  # 现场服务人员ID
             "logisticsDate": self.time[9],  # 物流完工时间
-            "logisticsIds": "13041158",  # 物流人员ID
+            "logisticsIds": "13041158,13041121",  # 物流人员ID
             "illustrate": "这里是项目的说明"  # 项目说明
         }
         response = self.request.post(url, headers=headers, json=data)
-        self.assert_util.assert_code(response['code'], 200)
-        print(f"项目创建成功，项目名称：{data['name']}")
+        self.assert_util.assert_in_body(response['code'], "'code': 40000")
+        print(f"项目创建成功，项目名称：{data['name']}, 返回body: {response['body']}")
 
     def get_contract_list(self):
         """ 获取合同列表 """
